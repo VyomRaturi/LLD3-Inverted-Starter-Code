eae92498cb85fc1db91c4de90b6ff178fc60507b
Vyom <vyomraturi2004@gmail.com>
2024-12-16 00:15:59 +0530

    minor

--- class-5/Movies.jsx
+++ class-5/Movies.jsx
@@ -44,11 +44,11 @@ function Movies() {
       {/* Pagination */}
       <div className="bg-gray-400 p-4 h-[50px] w-full mt-8 flex justify-center gap-2">
         <div className="px-8">
-          <i class="fa-solid fa-arrow-left"></i>
+          <i className="fa-solid fa-arrow-left"></i>
         </div>
         <div>{pageNo}</div>
         <div className="px-8">
-          <i class="fa-solid fa-arrow-right"></i>
+          <i className="fa-solid fa-arrow-right"></i>
         </div>
       </div>
     </div>
