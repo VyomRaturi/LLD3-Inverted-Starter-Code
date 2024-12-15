53fd2be35fc20bc808d6596795f62b353d5fe5ce
Vyom <vyomraturi2004@gmail.com>
2024-12-15 22:29:19 +0530

    minor change

--- class-5/Navbar.jsx
+++ class-5/Navbar.jsx
@@ -1,22 +1,6 @@
 import React from "react";
-import Logo from "../MovieLogo.png";
-import { Link } from "react-router-dom";
 function NavBar() {
-  return (
-    <div className="flex space-x-8 items-center pl-3 py-4">
-      <Link to="/">
-        <img className="w-[50px]" src={Logo} />
-      </Link>
-
-      <Link to="/" className="text-blue-500 text-3xl font-bold">
-        Movies
-      </Link>
-
-      <Link to="/watchlist" className="text-blue-500 text-3xl font-bold">
-        Watchlist
-      </Link>
-    </div>
-  );
+  return <div className="flex space-x-8 items-center pl-3 py-4">Navbar</div>;
 }
 
 export default NavBar;
