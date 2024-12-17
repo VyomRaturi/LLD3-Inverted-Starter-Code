7f485655f50908b819954780adc5e1864dd63d6e
Vyom <vyomraturi2004@gmail.com>
2024-12-17 12:17:00 +0530

    minor adjustment

--- class-5/Pagination.jsx
+++ class-5/Pagination.jsx
@@ -4,11 +4,11 @@ function Pagination() {
   return (
     <div className="bg-gray-400 p-4 h-[50px] w-full mt-8 flex justify-center">
       <div className="px-8">
-        <i class="fa-solid fa-arrow-left"></i>
+        <i className="fa-solid fa-arrow-left"></i>
       </div>
       <div>{/* Show page number here */}</div>
       <div className="px-8">
-        <i class="fa-solid fa-arrow-right"></i>
+        <i className="fa-solid fa-arrow-right"></i>
       </div>
     </div>
   );
